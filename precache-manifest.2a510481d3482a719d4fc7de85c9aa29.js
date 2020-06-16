cedbb5295caaabe8be04a096d9ec33a7785f3c80
Brad Smith <bradsmith712@gmail.com>
2020-06-16 09:44:53 -0500

    Updates

---
+++ precache-manifest.2a510481d3482a719d4fc7de85c9aa29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed8a3df1cb530b2a3d240db2d5d2a70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faed2c5e8f8c7bddd5fe",
+    "url": "/static/css/main.5b2bb298.chunk.css"
+  },
+  {
+    "revision": "e759b52f72551158b462",
+    "url": "/static/js/2.ef6c8607.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ef6c8607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faed2c5e8f8c7bddd5fe",
+    "url": "/static/js/main.231c4f05.chunk.js"
+  },
+  {
+    "revision": "1c4d17cf14649e6c3ad5",
+    "url": "/static/js/runtime-main.462339d7.js"
+  }
+]);
